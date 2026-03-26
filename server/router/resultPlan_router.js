418502aff676869c98ca47cc15b04d7aec98551d
kimdeokmo <sewonn201@naver.com>
2026-03-26 09:06:15 +0900

    '0326_김덕모_아침merge'

---
+++ server/router/resultPlan_router.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+
+const resultService = require("../service/result_service.js");
+
+// router.get("/resultList", async(req,res)=>{
+//   let list = await
+// })
+
+module.exports = router;
